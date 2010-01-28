155e61d5f6e11bb1ffb9444716190f60b7d8de0c
Henrik Thostrup Jensen <thostrup@gmail.com>
2010-01-28 14:30:12 +0100

    add setup file for source distribution and installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='lrmsurgen',
+      version='git',
+      description='LRMS UR Generator',
+      author='Henrik Thostrup Jensen',
+      author_email='htj@ndgf.org',
+      url='http://www.ndgf.org/',
+      packages=['lrmsurgen'],
+      scripts = ['lrms-ur-generator']
+)
+
