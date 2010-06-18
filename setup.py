7f41148768ab691bdbac1c302621103b2de3f207
Henrik Thostrup Jensen <thostrup@gmail.com>
2010-06-18 14:34:51 +0200

    add examples and autoversion to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from distutils.core import setup
 
+import time
+
+gmt = time.gmtime()
+version = 'git-%i%i%i' % (gmt.tm_year, gmt.tm_mon, gmt.tm_mday)
+
 setup(name='lrmsurgen',
-      version='git',
+      version=version,
       description='LRMS UR Generator',
       author='Henrik Thostrup Jensen',
       author_email='htj@ndgf.org',
       url='http://www.ndgf.org/',
       packages=['lrmsurgen'],
-      scripts = ['lrms-ur-generator']
+      package_data={'examples': ['examples/*']},
+      scripts = ['lrms-ur-generator', 'lrms-ur-registrant']
 )
 
